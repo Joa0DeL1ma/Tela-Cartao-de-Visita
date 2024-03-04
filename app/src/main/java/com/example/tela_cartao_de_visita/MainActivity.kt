f2a3c791b7307314c0c74573868e1cbfd1dba8b6
Joa0DeL1ma <161715327+Joa0DeL1ma@users.noreply.github.com>
2024-03-04 15:46:28 -0300

    Adicionado fundo a tela e funções de criação

--- app/src/main/java/com/example/tela_cartao_de_visita/MainActivity.kt
+++ app/src/main/java/com/example/tela_cartao_de_visita/MainActivity.kt
@@ -1,14 +1,21 @@
 package com.example.tela_cartao_de_visita
 
+import android.media.Image
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.tela_cartao_de_visita.ui.theme.TelaCartaodeVisitaTheme
 
@@ -22,7 +29,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    ComposeTela()
                 }
             }
         }
@@ -30,17 +37,37 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun ComposeTela() {
+    Column (
+        Modifier
+            .background(Color(0xFFA4C639))
+            .fillMaxSize()){
+        ComposableRow1(
+            mainImage = painterResource(id = R.drawable.kotlin_icon_svg),
+            name = stringResource(R.string.name),
+            titulo = stringResource(R.string.title)
+        )
+    }
+}
+
+@Composable
+fun ComposableRow1(
+    mainImage: Painter,
+    name: String,
+    titulo: String,
+    modifier: Modifier = Modifier) {
+}
+
+@Composable
+fun ComposableRow2(
+    name: String,
+    modifier: Modifier = Modifier) {
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     TelaCartaodeVisitaTheme {
-        Greeting("Android")
+        ComposeTela()
     }
 }
